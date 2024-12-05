2ced9ad86821a498c5f1e48d016961b9f2ed7428
M-Shourav <muhammadshourav625@gmail.com>
2024-12-05 10:34:16 +0600

    banner update

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -18,25 +18,28 @@ const Banner = () => {
           initial={{ y: 40, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.8 }}
-          className="px-8 flex flex-col gap-3 md:gap-5 items-start justify-between 
-            py-5 md:py-10 text-white"
+          className="px-8 flex flex-col gap-3 md:gap-5 items-start justify-center 
+            py-5 md:py-10 text-white mb-2 md:mb-0 absolute md:top-20 md:left-0"
         >
-          <h2 className="text-lg lg:text-6xl font-semibold max-w-[400px]">
+          <h2 className="text-lg md:text-2xl lg:text-6xl font-semibold max-w-[400px]">
             Broad Vision Honest Service Great Value
           </h2>
           <p className="text-sm md:text-base">
             Our goal then and now is to provide quality on time projects
           </p>
           <div className="flex items-center gap-5">
-            <Button href="/">
+            <Button
+              href="/"
+              className="py-1.5 md:py-2 lg:py-2.5 text-sm md:text-base "
+            >
               <span className="mt-1">
                 <FaArrowRightLong />
               </span>
               Get Free Quote
             </Button>
             <Button
-              className="px-4 py-2.5 md:py-4 rounded-sm flex items-center gap-1 
-            text-base font-semibold duration-300
+              className="py-1.5 md:py-2 lg:py-2.5  rounded-sm flex items-center gap-1 
+            text-sm md:text-base font-semibold duration-300
             border border-transparent hover:border-gray-300"
             >
               Get In Touch
